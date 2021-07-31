e71e435d2775f42af7d0a331d1416a6053c6e0fb
krisjwood <kriswd0@gmail.com>
2021-08-01 11:23:29 +1200

    add item input field

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import React, { useState, useEffect } from 'react'
+
 function App() {
+  const [input, setInput] = useState('')
+  const [list, setList] = useState([])
+
+  function handleSubmit (e) {}
+
+  
+  function handleChange (e) {
+    const newItem = e.target.value
+    setInput(newItem)
+  }
+
   return (
-  <h1>Hello, world!</h1>
+    <>
+    <h1>Kris&#39; Shopping List</h1>
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Add item to list"
+        autoFocus={true}
+        onChange={handleChange}
+        value={input}
+      />
+      <button>Add</button>
+    </form>
+    </>
   )
 }
 
