8ce85da7abfea6d7a3a106e9e422ad9219d167fc
krisjwood <kriswd0@gmail.com>
2021-08-01 11:42:07 +1200

    create list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 function App() {
   const [input, setInput] = useState('')
-  const [list, setList] = useState([])
+  const [list, setList] = useState([
+    { id: 1, timeStamp: 1, name: 'item 1', complete: false },
+    { id: 2, timeStamp: 2, name: 'item 2', complete: true }
+  ])
 
-  function handleSubmit (e) {}
+  function handleSubmit (e) {
+    e.preventDefault()
+    const newList = [
+      ...list,
+      {
+        timeStamp: Date.now(),
+        name: input,
+        complete: false
+      }
+    ]
+    setList(newList)
+    // POST name
+    setInput('')
+  }
 
-  
+
   function handleChange (e) {
     const newItem = e.target.value
     setInput(newItem)
+  }
+
+  function clickDelete(id) {
+    console.log(id)
+  }
+
+  function toggleComplete(id) {
+    console.log(id)
   }
 
   return (
@@ -24,6 +48,14 @@ function App() {
       />
       <button>Add</button>
     </form>
+    <ul>
+        {list.map(item =>
+          <li key={item.timeStamp} className={item.complete ? 'completed' : ''}>
+            <input className="toggle" type="checkbox" checked={item.complete} onChange={() => toggleComplete(item.timeStamp)} value={item.timeStamp} />
+            {item.name} <button onClick={() => clickDelete(item.timeStamp)} >Delete</button></li>
+        )}
+      </ul>
+      <p>&copy;2021 Kris Wood</p>
     </>
   )
 }
